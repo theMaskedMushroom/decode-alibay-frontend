c75cd9dc2fbebeb728eeab69ac2c08dd199cfc1f
baboma <munungu.andy@gmail.com>
2018-11-07 13:52:41 -0500

    First commit

---
+++ src/ItemList.js
@@ -0,0 +1,65 @@
+
+import React, { Component } from 'react';
+import Product from './Item';
+
+
+/*var sellers = [{
+  name: "Jack Frost",
+  rating: "5 stars"
+}, {
+  name: "Hank Green",
+  rating: "2 stars"
+}, {
+  name: "Donald Trump",
+  rating: "6 stars"
+}
+];*/
+
+class ItemList extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      products: []
+    }
+    this.getAllProducts = this.getAllProducts.bind(this);
+  }
+
+  // Get all the products from DB
+  getAllProducts(){
+    fetch('/products', {
+      method: 'GET'
+    }).then(x => x.text())
+    .then(res => {
+      var parsed = JSON.parse(res);
+      if(parsed.status){
+        this.setState({products: parsed.products});
+      }
+    })
+  }
+  //componentDidMount() {
+  //  this.getAllProducts();
+  //}
+
+  render(){
+    this.getAllProducts();
+    
+    const productComponents = this.state.products.map((product, ind) => (
+      <Product 
+        key={'AL' + ind}
+        id={product.id}
+        name={product.name}
+        price={product.price}
+        description={product.description}        
+        imageUrl={product.imageUrl}
+        vendor={product.vendor_id}
+      />
+    ));
+    
+    return (
+    <div>
+      {productComponents}
+    </div>);
+  }
+}
+
+export default ItemList;
